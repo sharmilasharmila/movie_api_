b738570043390f05e288f47d7b0582eaf35ea0a7
sajith95 <sra084@post.uit.no>
2021-03-10 12:12:46 +0530

    Final Update

--- models.js
+++ models.js
@@ -22,7 +22,7 @@ let userSchema = mongoose.Schema({
   Password: {type: String, required: true},
   Email: {type: String, required: true},
   Birthday: Date,
-  fFavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
 userSchema.statics.hashPassword = (password) => {
