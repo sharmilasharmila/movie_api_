7ab23446182abba07dd8a5699edd503af003e9f6
sajith95 <sra084@post.uit.no>
2021-03-16 08:51:07 +0530

    Update Index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
 });
 
 // Returns a list of all movies
-app.get('/movies', (req, res) => {
+app.get('/movies',passport.authenticate('jwt', { session: false }), (req, res) => {
     Movies.find()
         .then((movies) => {
             res.status(201).json(movies);
