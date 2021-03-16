de98289408bd9786965ea4517a782c229673837a
sajith95 <sra084@post.uit.no>
2021-03-16 08:27:09 +0530

    Add CORS

--- auth.js
+++ auth.js
@@ -18,6 +18,7 @@ let generateJWTToken = (user) => {
 /* POST login. */
 module.exports = (router) => {
   router.post('/login', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
